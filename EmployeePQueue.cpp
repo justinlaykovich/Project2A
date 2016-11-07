14b98a3fe729216d8012819e3fbb9aac406f20f9
justinlaykovich <jdlb8d@mail.umkc.edu>
2016-11-07 13:13:27 -0600

    Ah

--- EmployeePQueue.cpp
+++ EmployeePQueue.cpp
@@ -1,4 +1,4 @@
-#include"PQueue.h"
+#include"EmployeePQueue.h"
 #include<stdexcept>
 #include<algorithm>
 #include<iostream>
@@ -6,21 +6,18 @@
 using std::cout;
 using std::endl;
 
-template<class T>
-PriorityQueue<T>::PriorityQueue() {
+EmployeeEmployeePQueue::EmployeePQueue() {
    size = 0;
 }
 
-template<class T>
-PriorityQueue<T>::PriorityQueue(const std::vector<T>& inserted) {
+EmployeePQueue::EmployeePQueue(const std::vector<Employee>& inserted) {
    size = 0;
    int insertSize = inserted.size();
    for(int i = 0; i < insertSize; i++)
       insert(inserted[i]);
 }
 
-template<class T>
-void PriorityQueue<T>::insert(const T& item) {
+void EmployeePQueue::insert(const Employee& item) {
    data.push_back(item);
    size++;
 
@@ -31,13 +28,11 @@ void PriorityQueue<T>::insert(const T& item) {
          break;
 }
 
-template<class T>
-void PriorityQueue<T>::max_heapify() {
+void EmployeePQueue::max_heapify() {
    max_heapify(0);
 }
 
-template<class T>
-void PriorityQueue<T>::max_heapify(int i) {
+void EmployeePQueue::max_heapify(int i) {
    int left = 2*i + 1;
    int right = 2*i + 2;
    int largest = i;
@@ -55,20 +50,17 @@ void PriorityQueue<T>::max_heapify(int i) {
    }
 }
 
-template<class T>
-T PriorityQueue<T>::extract_max() {
-   T item = data[0];
+Employee EmployeePQueue::extract_max() {
+   Employee item = data[0];
    remove(0);
    return item;
 }
 
-template<class T>
-T PriorityQueue<T>::top() {
+Employee EmployeePQueue::top() {
    return data[0];
 }
 
-template<class T>
-void PriorityQueue<T>::remove(const int& index){
+void EmployeePQueue::remove(const int& index){
    if(index >= size || index < -1)
       throw;
 
@@ -78,13 +70,11 @@ void PriorityQueue<T>::remove(const int& index){
    max_heapify(index);
 }
 
-template<class T>
-bool PriorityQueue<T>::is_empty() {
+bool EmployeePQueue::is_empty() {
    return size == 0;
 }
 
-template<class T>
-void PriorityQueue<T>::invalidate(const T& item) {
+void EmployeePQueue::invalidate(const Employee& item) {
    for(int i = 0; i < size; i++)
       if(data[i] == item) {
          cout << "Invalidated." << endl;

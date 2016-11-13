da6ec9a8e9910950e0bb6ba81ba64a276756ffd9
justinlaykovich <jdlb8d@mail.umkc.edu>
2016-11-13 16:05:23 -0600

    a

--- Book.h
+++ Book.h
@@ -3,6 +3,11 @@
 
 class Book {
 public:
+   Book(const string&);
+   Employee* circulate(Date,std::vector<Employee>);
+   Employee pass_on(Date);
+   Employee* get_current_employee();
+   const string& get_name() const;
    string name;
    Date circulation_start_date;
    Date circulation_end_date;
@@ -11,7 +16,7 @@ public:
    Date circulation_last_date;
 
    /* Keeps track of employee currently holding book */
-   Employee current_employee;
+   Employee* current_employee;
    bool archived = true;
    PriorityQueue<Employee> waiting_list;
 
@@ -20,5 +25,46 @@ public:
       return name == rhs.name;
    }
 };
+
+Book::Book(const string& name) {
+   this->name = name;
+}
+
+const string& Book::get_name() const {
+   return name;
+}
+
+Employee* Book::circulate(Date date,std::vector<Employee> employees) {
+   circulation_start_date = date;
+   circulation_last_date = date;
+   archived = false;
+   waiting_list = PriorityQueue<Employee>(employees);
+   *current_employee = waiting_list.extract_max();
+   current_employee->books_possessed += 1;
+   return get_current_employee();
+}
+
+Employee Book::pass_on(Date date) {
+   Employee last_employee = *current_employee;
+   last_employee.retaining_time += date - (circulation_last_date);
+   last_employee.books_possessed -= 1;
+
+   if(!waiting_list.is_empty()) {
+      circulation_last_date = date;
+      *current_employee = waiting_list.extract_max();
+      current_employee->waiting_time += date - circulation_start_date;
+      current_employee->books_possessed += 1;
+   }
+   else {
+      circulation_end_date = date;
+      archived = true;
+   }
+
+   return last_employee;
+}
+
+Employee* Book::get_current_employee() {
+   return &(*current_employee);
+}
 
 #endif
